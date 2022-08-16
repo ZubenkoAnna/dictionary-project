448ef4cff89ec6acf947f92720ee5138512e25e1
ZubenkoAnna <zubenkoweb@gmail.com>
2022-08-16 15:06:55 +0300

    Added file and bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <button className="btn btn-success">Search</button>
+      </header>
+    </div>
+  );
+}
