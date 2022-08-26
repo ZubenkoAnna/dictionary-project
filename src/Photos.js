0078f683f4375d6b58f776b5211e67a1270c9924
ZubenkoAnna <zubenkoweb@gmail.com>
2022-08-26 14:01:18 +0300

    Response img

--- src/Photos.js
+++ src/Photos.js
@@ -5,13 +5,17 @@ import "./Photos.css";
 export default function Photos(props) {
   if (props.photo) {
     return (
-      <section>
+      <section className="section">
         <div className="row">
           {props.photo.photos.map(function (photos, index) {
             return (
-              <div className="col-4" key={index}>
+              <div className="col-6" key={index}>
                 <a href={photos.url} target="_blank" rel="noreferrer">
-                  <img src={photos.src.landscape} />
+                  <img
+                    src={photos.src.landscape}
+                    className="img-fluid"
+                    alt="example"
+                  />
                 </a>
               </div>
             );
